62334ed5c912eecd790c4853d940046b7ce40f44
akshatdas <akshat.das@berkeley.edu>
2017-07-02 21:15:10 -0700

    Clean

--- twitterBot.py
+++ twitterBot.py
@@ -5,10 +5,10 @@ import sys, time, tweepy
 
 argfile = str(sys.argv[1])
 
-CONSUMER_KEY = 'or6E5EkRhI3ehTOYb9R1Fs2ah'
-CONSUMER_SECRET = 'XKmNTNsvVi4l4eWQ17J7ckFCfHjIDJAt9ioaVypLD0832EPbPu'
-ACCESS_KEY = '271432745-wXdu6X77Mb8BRDcuHOiP7Phc8sZPsk8wWfwtKfGt'
-ACCESS_SECRET = 'RjbKMJt0MLJgRSSYRmUJX73aiQ0GOM5RSPBzmGkIP8d6V'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
